9e5afbfe13e4db63a5329b626f7faddf6fe7bf15
skuma <skumazin@gmail.com>
2022-06-13 22:25:14 +0200

    test

--- user_input.py
+++ user_input.py
@@ -2,8 +2,14 @@
 # print(name)
 # print("My name is " + name)
 
-size_input = input("How big is your house (in square feet): ")
-square_feet = int(size_input)
-square_meters = square_feet / 10.8
-print(square_meters)
-print(f"{square_feet} square feet is {square_meters:.2f} square meters")
+# size_input = input("How big is your house (in square feet): ")
+# square_feet = int(size_input)
+# square_meters = square_feet / 10.8
+# print(square_meters)
+# print(f"{square_feet} square feet is {square_meters:.2f} square meters")
+
+
+user_age = input("Enter your age: ")
+years = int(user_age)
+age_in_months = years * 12
+print(f"Your age is {years} is equal to {age_in_months}")
